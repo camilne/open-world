7538bad6b44bb7c423aaf6c1007d177b9898b075
Longarmx <cameronm213@gmail.com>
2017-04-12 12:56:34 -0600

    Added automatic calculation of region normals

--- src/com/camilne/world/Region.java
+++ src/com/camilne/world/Region.java
@@ -10,7 +10,7 @@ import com.camilne.rendering.Vertex;
 
 public class Region {
     
-    private static final int SIZE = 32;
+    private static final int SIZE = 2;
     
     private Mesh terrainMesh;
     private Matrix4f transformationMatrix;
@@ -27,31 +27,61 @@ public class Region {
     }
     
     private void createTerrain() {
-	Vertex[] vertices = new Vertex[(SIZE + 1) * (SIZE + 1) * 4];
+	Vertex[] vertices = new Vertex[SIZE * SIZE * 4];
 	int idx = 0;
-	for(int j = 0; j < SIZE + 1; j++) {
-	    for(int i = 0; i < SIZE + 1; i++) {
-		vertices[idx++] = new Vertex(new Vector3f(i, 0, j),		new Vector2f(0, 0), new Vector3f(0, 1, 0));
-		vertices[idx++] = new Vertex(new Vector3f(i + 1, 0, j),		new Vector2f(1, 0), new Vector3f(0, 1, 0));
-		vertices[idx++] = new Vertex(new Vector3f(i + 1, 0, j + 1),	new Vector2f(1, 1), new Vector3f(0, 1, 0));
-		vertices[idx++] = new Vertex(new Vector3f(i, 0, j + 1),		new Vector2f(0, 1), new Vector3f(0, 1, 0));
+	for(int j = 0; j < SIZE; j++) {
+	    for(int i = 0; i < SIZE; i++) {
+		vertices[idx++] = new Vertex(new Vector3f(i, 0, -j),		new Vector2f(0, 0), new Vector3f(0, 1, 0));
+		vertices[idx++] = new Vertex(new Vector3f(i + 1, 0, -j),	new Vector2f(1, 0), new Vector3f(0, 1, 0));
+		vertices[idx++] = new Vertex(new Vector3f(i + 1, 0, -(j + 1)),	new Vector2f(1, 1), new Vector3f(0, 1, 0));
+		vertices[idx++] = new Vertex(new Vector3f(i, 0, -(j + 1)),	new Vector2f(0, 1), new Vector3f(0, 1, 0));
 	    }
 	}
-	int[] indices = new int[(SIZE + 1) * (SIZE + 1) * 6];
+	
+	for(int i = 0; i < vertices.length; i++) {
+	    switch(i % 4) {
+	    case 0:
+		if(i + 3 < vertices.length) {
+		    calculateNormal(vertices[i], vertices[i + 1], vertices[i + 3]);
+		}
+		break;
+	    case 1:
+	    case 2:
+		if(i + 1 < vertices.length && i - 1 >= 0) {
+		    calculateNormal(vertices[i], vertices[i + 1], vertices[i - 1]);
+		}
+		break;
+	    case 3:
+		if(i - 3 >= 0) {
+		    calculateNormal(vertices[i], vertices[i - 3], vertices[i - 1]);
+		}
+		break;
+	    }
+	}
+	
+	int[] indices = new int[SIZE * SIZE * 6];
 	int index = 0;
 	int indicesIndex = 0;
-	for(int j = 0; j < SIZE + 1; j++) {
-	    for(int i = 0; i < SIZE + 1; i++) {
-		indices[indicesIndex++] = index + 2;
+	for(int j = 0; j < SIZE; j++) {
+	    for(int i = 0; i < SIZE; i++) {
+		indices[indicesIndex++] = index + 0;
 		indices[indicesIndex++] = index + 1;
-		indices[indicesIndex++] = index + 0;
-		indices[indicesIndex++] = index + 0;
-		indices[indicesIndex++] = index + 3;
 		indices[indicesIndex++] = index + 2;
+		indices[indicesIndex++] = index + 2;
+		indices[indicesIndex++] = index + 3;
+		indices[indicesIndex++] = index + 0;
 		index += 4;
 	    }
 	}
+	
 	terrainMesh = new Mesh(vertices, indices, false);
     }
 
+    private void calculateNormal(Vertex src, Vertex v1, Vertex v2) {
+	Vector3f a = Vector3f.sub(v1.getPos(), src.getPos(), null);
+	Vector3f b = Vector3f.sub(v2.getPos(), src.getPos(), null);
+	
+	Vector3f.cross(a, b, src.getNormal()).normalise();
+    }
+    
 }
