3cbf8f80dfdbec88349785426f2a80cfca5a0495
Longarmx <cameronm213@gmail.com>
2017-05-03 17:06:01 -0600

    Changed water texture coordinate calculation

--- res/shaders/water.fs
+++ res/shaders/water.fs
@@ -1,5 +1,6 @@
 #version 330
 
+in vec4 clip_space;
 in vec2 f_texcoords;
 
 out vec4 final_color;
@@ -8,8 +9,9 @@ uniform sampler2D reflection_texture;
 
 void main()
 {
-	vec2 screen_space = vec2(gl_FragCoord.x / 1280.0, 1 - gl_FragCoord.y / 720.0);
-	vec4 reflect_color = texture(reflection_texture, screen_space);
+	vec2 ndc = (clip_space.xy/clip_space.w) / 2.0 + 0.5;
+
+	vec4 reflect_color = texture(reflection_texture, vec2(ndc.x, -ndc.y));
 
 	final_color = reflect_color * vec4(0.8, 0.8, 0.8, 0.5);
 }
--- res/shaders/water.vs
+++ res/shaders/water.vs
@@ -4,6 +4,7 @@ layout(location = 0) in vec3 v_position;
 layout(location = 1) in vec2 v_texcoords;
 layout(location = 2) in vec3 v_normal;
 
+out vec4 clip_space;
 out vec2 f_texcoords;
 
 uniform mat4 m_proj;
@@ -13,7 +14,8 @@ uniform mat4 m_model;
 void main()
 {
 	vec4 world_pos = m_model * vec4(v_position, 1.0);
-	gl_Position = m_proj * m_view * world_pos;
+	clip_space = m_proj * m_view * world_pos;
+	gl_Position = clip_space;
 	
 	f_texcoords = v_texcoords;
 }
